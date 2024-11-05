89526f480470f6bc23db1a9e51469dbddd338885
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-05 13:36:11 +0900

    Add files via upload

---
+++ vius7be.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/LineageW/02_main_f.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
